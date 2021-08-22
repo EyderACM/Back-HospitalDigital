4abbd51957a59a6238a7d4805eb8670d69e2cddc
eyderacm <eyderacm@gmail.com>
2021-08-22 17:07:44 -0500

    feat: initialize router

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 
+import patientRoutes from "./routes/patient.routes";
+
 const app = express();
 
-//middlewares
+// middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+// routes
+app.use(patientRoutes);
 
 app.listen(3000);
 console.log("Server on port", 3000);
