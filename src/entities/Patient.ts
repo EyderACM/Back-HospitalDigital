3795496f36b5c7637e49432d8b82c570935d7c8f
eyderacm <eyderacm@gmail.com>
2021-08-22 21:09:39 -0500

    feat: add patient entity

---
+++ src/entities/Patient.ts
@@ -0,0 +1,52 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { IsDate, IsInt, Max, Min } from "class-validator";
+import { Type } from "class-transformer";
+
+@Entity()
+export class Patient extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  @IsInt()
+  @Min(0)
+  @Max(100)
+  age: number;
+
+  @Column()
+  sex: string;
+
+  @Column({ type: "date" })
+  @Type(() => Date)
+  @IsDate()
+  birth_date: Date;
+
+  @Column()
+  city_name: string;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    onUpdate: "CURRENT_TIMESTAMP(6)",
+  })
+  updated_at: Date;
+}
