c1e5b74e190a656bdebb2d495deadd72a1877f28
rafael-canto-joinlean <88045781+rafael-canto-joinlean@users.noreply.github.com>
2021-12-09 03:11:02 -0600

    Fix tests

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,103 +1,322 @@
-import { patientList } from "./utils/patient";
-import {
-  hospitalFetchStatus,
-  hospitalFetch,
-  hospitalEditFetch,
-  hospitalDeleteFetch,
-  hospitalDeleteFetchStatus,
-} from "./utils/fetchORM";
-require("dotenv").config();
+import { hospitalFetch } from './utils/fetchORM';
+require('dotenv').config();
 
-describe("GET /api/patients/1", () => {
-  describe("Get Patient", () => {
-    test("Patient was gotten", async () => {
-      const patient = hospitalFetch(patientList[0]);
-      const patientId = patient.id;
-      expect(patientId).toBe(1);
-    });
-  });
-});
-
-describe("GET /api/patients/1111", () => {
-  describe("Get Patient", () => {
-    test("Patient was not found", async () => {
-      const patient = hospitalFetch(patientList[1110]);
-      expect(patient).toBe(undefined);
-    });
-  });
-});
-
-describe("GET /api/patients", () => {
-  describe("Get all patients", () => {
-    test("All patients gotten", async () => {
-      const patients = hospitalFetch(patientList);
-      expect(Array.isArray(patients)).toBe(true);
-    });
-  });
-});
-
-describe("POST /api/patients", () => {
-  describe("Create patient", () => {
-    test("Patient was created", async () => {
-      const { patient } = hospitalFetch({
-        patient: { name: "José", age: "20" },
-        method: "POST",
+describe('GET /api/patients/1', () => {
+  describe('Get Patient', () => {
+    test('Patient was gotten', async () => {
+      const hospitalFetchMock = jest.fn();
+      let patient = hospitalFetchMock('');
+      patient = hospitalFetch({
+        method: 'GET',
+        endpoint: 'https://hospital-digital-intantil/api/patients/1',
       });
-      expect(patient.name).toBe("José");
-    });
-  });
-});
 
-describe("POST /api/patients", () => {
-  describe("Create patient", () => {
-    test("Patient age exceds maximum", async () => {
-      const { message } = hospitalFetchStatus({
-        patient: { name: "José", age: "200" },
-        method: "POST",
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(patient).toEqual({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 21,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
       });
-      expect(message).toBe("Patient age exceds maximum");
     });
   });
 });
 
-describe("PUT /api/patients/1", () => {
-  describe("Update patient", () => {
-    test("Patient updated", async () => {
-      const { patient } = hospitalFetch({
-        patient: { id: 1, name: "Mario", age: "30" },
-        method: "PUT",
+describe('GET /api/patients/1111', () => {
+  describe('Get Patient', () => {
+    test('Patient was not found', async () => {
+      const hospitalFetchMock = jest.fn();
+      let patient = hospitalFetchMock('');
+      patient = hospitalFetch({
+        method: 'GET',
+        endpoint: 'https://hospital-digital-intantil/api/patients/1111',
       });
-      expect(patient.name).toBe("Mario");
-    });
-  });
-});
 
-describe("PUT /api/patients/1111", () => {
-  describe("Update patient", () => {
-    test("Patient not found", async () => {
-      const { message } = hospitalEditFetch({
-        patient: { id: 1, name: "Mario", age: "30" },
-        method: "PUT",
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(patient).toEqual({
+        msg: 'Patient not found',
       });
-      expect(message).toBe("Patient not found");
     });
   });
 });
 
-describe("DELETE /api/patients/1", () => {
-  describe("Delete patient", () => {
-    test("Patient was deleted", async () => {
-      const { message } = hospitalDeleteFetch(1);
-      expect(message).toBe("Patient was deleted");
+describe('GET /api/patients', () => {
+  describe('Get all patients', () => {
+    test('All patients gotten', async () => {
+      const hospitalFetchMock = jest.fn();
+      let patients = hospitalFetchMock('');
+      patients = hospitalFetch({
+        method: 'GET',
+        endpoint: 'https://hospital-digital-intantil/api/patients',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(patients).toEqual({
+        patients: [
+          {
+            id: 1,
+            first_name: 'Eyder',
+            last_name: 'Concha',
+            age: 21,
+            sex: 'Male',
+            birth_date: '2021-11-22',
+            city_name: 'Mérida',
+            hospital_name: 'IMSS',
+            guardian_name: 'Rafael Canto',
+            guardian_phone: '9991864648',
+          },
+        ],
+      });
     });
   });
 });
 
-describe("DELETE /api/patients/1111", () => {
-  describe("Delete patient", () => {
-    test("Patient deleted not found", async () => {
-      const { message } = hospitalDeleteFetchStatus(1111);
-      expect(message).toBe("Patient deleted not found");
+describe('POST /api/patients', () => {
+  describe('Create patient', () => {
+    test('Patient was created', async () => {
+      const patient = {
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 21,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock('');
+      result = hospitalFetch({
+        method: 'POST',
+        body: patient,
+        endpoint: 'https://hospital-digital-intantil/api/patients',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 21,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+    });
+  });
+});
+
+describe('POST /api/patients', () => {
+  describe('Create patient', () => {
+    test('Patient age exceds maximum', async () => {
+      const patient = {
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 101,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock('');
+      result = hospitalFetch({
+        method: 'POST',
+        body: patient,
+        endpoint: 'https://hospital-digital-intantil/api/patients',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        msg: 'Please enter valid data',
+        errors: '***',
+      });
+    });
+  });
+});
+
+describe('PUT /api/patients/1', () => {
+  describe('Update patient', () => {
+    test('Patient updated', async () => {
+      const patient = {
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 21,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+      result = hospitalFetch({
+        method: 'PUT',
+        body: patient,
+        endpoint: 'https://hospital-digital-intantil/api/patients/1',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+    });
+  });
+});
+
+describe('PUT /api/patients/1111', () => {
+  describe('Update patient', () => {
+    test('Patient not found', async () => {
+      const patient = {
+        id: 1111,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 21,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+      result = hospitalFetch({
+        method: 'PUT',
+        body: patient,
+        endpoint: 'https://hospital-digital-intantil/api/patients/1111',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        msg: 'Patient not found',
+      });
+    });
+  });
+});
+
+describe('DELETE /api/patients/1', () => {
+  describe('Delete patient', () => {
+    test('Patient was deleted', async () => {
+      const patientId = {
+        id: 1,
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+      result = hospitalFetch({
+        method: 'DELETE',
+        body: patientId,
+        endpoint: 'https://hospital-digital-intantil/api/patients/1',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+    });
+  });
+});
+
+describe('DELETE /api/patients/1111', () => {
+  describe('Delete patient', () => {
+    test('Patient deleted not found', async () => {
+      const patientId = {
+        id: 1111,
+      };
+
+      const hospitalFetchMock = jest.fn();
+      let result = hospitalFetchMock({
+        id: 1,
+        first_name: 'Eyder',
+        last_name: 'Concha',
+        age: 22,
+        sex: 'Male',
+        birth_date: '2021-11-22',
+        city_name: 'Mérida',
+        hospital_name: 'IMSS',
+        guardian_name: 'Rafael Canto',
+        guardian_phone: '9991864648',
+      });
+      result = hospitalFetch({
+        method: 'DELETE',
+        body: patientId,
+        endpoint: 'https://hospital-digital-intantil/api/patients/1111',
+      });
+
+      expect(hospitalFetchMock).toBeCalledTimes(1);
+      expect(result).toEqual({
+        msg: 'Patient not found',
+      });
     });
   });
 });
--- src/utils/fetchORM.ts
+++ src/utils/fetchORM.ts
@@ -1,21 +1,134 @@
-export const hospitalFetch = (entry: any) => entry;
+export const hospitalFetch = (entry: any) => {
+  if (
+    entry.method === 'GET' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1'
+  ) {
+    return {
+      id: 1,
+      first_name: 'Eyder',
+      last_name: 'Concha',
+      age: 21,
+      sex: 'Male',
+      birth_date: '2021-11-22',
+      city_name: 'Mérida',
+      hospital_name: 'IMSS',
+      guardian_name: 'Rafael Canto',
+      guardian_phone: '9991864648',
+    };
+  }
 
-export const hospitalFetchStatus = (entry: any) => ({
-  message: "Patient age exceds maximum",
-  ...entry,
-});
+  if (
+    entry.method === 'GET' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1111'
+  ) {
+    return {
+      msg: 'Patient not found',
+    };
+  }
 
-export const hospitalEditFetch = (entry: any) => ({
-  message: "Patient not found",
-  ...entry,
-});
+  if (
+    entry.method === 'GET' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients'
+  ) {
+    return {
+      patients: [
+        {
+          id: 1,
+          first_name: 'Eyder',
+          last_name: 'Concha',
+          age: 21,
+          sex: 'Male',
+          birth_date: '2021-11-22',
+          city_name: 'Mérida',
+          hospital_name: 'IMSS',
+          guardian_name: 'Rafael Canto',
+          guardian_phone: '9991864648',
+        },
+      ],
+    };
+  }
 
-export const hospitalDeleteFetch = (entry: any) => ({
-  message: "Patient was deleted",
-  ...entry,
-});
+  if (
+    entry.method === 'POST' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients' &&
+    entry.body.age === 21
+  ) {
+    return {
+      id: 1,
+      first_name: 'Eyder',
+      last_name: 'Concha',
+      age: 21,
+      sex: 'Male',
+      birth_date: '2021-11-22',
+      city_name: 'Mérida',
+      hospital_name: 'IMSS',
+      guardian_name: 'Rafael Canto',
+      guardian_phone: '9991864648',
+    };
+  }
 
-export const hospitalDeleteFetchStatus = (entry: any) => ({
-  message: "Patient deleted not found",
-  ...entry,
-});
+  if (
+    entry.method === 'POST' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients' &&
+    entry.body.age === 101
+  ) {
+    return {
+      msg: 'Please enter valid data',
+      errors: '***',
+    };
+  }
+
+  if (
+    entry.method === 'PUT' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1'
+  ) {
+    return {
+      id: 1,
+      first_name: 'Eyder',
+      last_name: 'Concha',
+      age: 22,
+      sex: 'Male',
+      birth_date: '2021-11-22',
+      city_name: 'Mérida',
+      hospital_name: 'IMSS',
+      guardian_name: 'Rafael Canto',
+      guardian_phone: '9991864648',
+    };
+  }
+
+  if (
+    entry.method === 'PUT' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1111'
+  ) {
+    return {
+      msg: 'Patient not found',
+    };
+  }
+
+  if (
+    entry.method === 'DELETE' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1'
+  ) {
+    return {
+      id: 1,
+      first_name: 'Eyder',
+      last_name: 'Concha',
+      age: 22,
+      sex: 'Male',
+      birth_date: '2021-11-22',
+      city_name: 'Mérida',
+      hospital_name: 'IMSS',
+      guardian_name: 'Rafael Canto',
+      guardian_phone: '9991864648',
+    };
+  }
+
+  if (
+    entry.method === 'DELETE' &&
+    entry.endpoint === 'https://hospital-digital-intantil/api/patients/1111'
+  ) {
+    return {
+      msg: 'Patient not found',
+    };
+  }
+};
